267c36e343101cff96913602611f8a7f875b64d4
Jeremy Kevin Alexandro Sitorus <sitoruskevin52@gmail.com>
2022-10-26 11:26:19 +0700

    changes

---
+++ book_purchasing.js
@@ -0,0 +1,25 @@
+function bookPurchasing(diskon, buku, task)
+{
+    let result = 0;
+    if(buku && buku.harga && buku.judul)
+    {
+        const hrg = buku.harga;
+        const jlhDsc  = hrg *(diskon/100);
+        const hargaDsc = hrg - jlhDsc;
+        const pjk = hargaDsc * (tax/100);
+        const hargaPjk = hargaDsc - pjk;
+
+        console.group();
+        console.log('Judul Buku                    :', buku.judul);
+        console.log('Harga Barang                  : Rp.', hrg);
+        console.log('Discount                      :', diskon,'%');
+        console.log('pajak                         :', tax,'%');
+        console.log('Harga Barang setelah discount : Rp.', hargaDsc);
+        console.log('Harga Barang dipotong pajak   : Rp.', hargaPjk);
+        console.log('Harga total                   : Rp.', hargaPjk);
+        console.groupEnd();
+        
+    }
+    return result;
+}
+bookPurchasing({judul: 'terbang', harga: 10000, printing_status: true}, 10, 10);
