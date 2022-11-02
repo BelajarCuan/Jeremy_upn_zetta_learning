747237c3e97413609630639f0db3eda7774086fd
Jeremy Kevin Alexandro Sitorus <sitoruskevin52@gmail.com>
2022-11-02 15:38:38 +0700

    changes

--- angular_learning/src/app/app-routing.module.ts
+++ angular_learning/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 
+const routes: Routes = [
+
+  {
+    path :'kasir',
+    loadChildren:()=>import('./kasir/kasir.module').then(m=> m.KasirModule),
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
